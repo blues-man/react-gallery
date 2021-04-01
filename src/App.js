728b8c04ff8e2119492e033f66dbc6124b33f2aa
Natale Vinto <nvinto@redhat.com>
2021-04-01 18:27:30 +0200

    Change structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,511 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React from 'react';
+import {
+  Avatar,
+  Brand,
+  Button,
+  ButtonVariant,
+  Card,
+  CardActions,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Checkbox,
+  Divider,
+  Dropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownSeparator,
+  DropdownPosition,
+  DropdownDirection,
+  DropdownToggleCheckbox,
+  Drawer,
+  DrawerActions,
+  DrawerPanelBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerContentBody,
+  DrawerHead,
+  DrawerPanelContent,
+  DrawerSection,
+  Flex,
+  FlexItem,
+  Gallery,
+  GalleryItem,
+  KebabToggle,
+  Nav,
+  NavItem,
+  NavList,
+  NavVariants,
+  OverflowMenu,
+  OverflowMenuControl,
+  OverflowMenuGroup,
+  OverflowMenuItem,
+  Page,
+  PageHeader,
+  PageSection,
+  PageSectionVariants,
+  PageSidebar,
+  Pagination,
+  Progress,
+  Select,
+  SelectVariant,
+  SelectOption,
+  SkipToContent,
+  TextContent,
+  Text,
+  Title,
+  Toolbar,
+  ToolbarItem,
+  ToolbarContent,
+  ToolbarFilter,
+  ToolbarToggleGroup
+} from '@patternfly/react-core';
+
+import BellIcon from '@patternfly/react-icons/dist/js/icons/bell-icon';
+import CogIcon from '@patternfly/react-icons/dist/js/icons/cog-icon';
+import FilterIcon from '@patternfly/react-icons/dist/js/icons/filter-icon';
+import TrashIcon from '@patternfly/react-icons/dist/js/icons/trash-icon';
+import imgBrand from '@patternfly/react-core/src/components/Brand/examples/pfLogo.svg';
+import imgAvatar from '@patternfly/react-core/src/components/Avatar/examples/avatarImg.svg';
+import pfIcon from './pf-logo-small.svg';
+import activeMQIcon from './activemq-core_200x150.png';
+import avroIcon from './camel-avro_200x150.png';
+import dropBoxIcon from './camel-dropbox_200x150.png';
+import infinispanIcon from './camel-infinispan_200x150.png';
+import saxonIcon from './camel-saxon_200x150.png';
+import sparkIcon from './camel-spark_200x150.png';
+import swaggerIcon from './camel-swagger-java_200x150.png';
+import azureIcon from './FuseConnector_Icons_AzureServices.png';
+import restIcon from './FuseConnector_Icons_REST.png';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 1,
+      perPage: 10,
+      isDrawerExpanded: false,
+      activeCard: null,
+      filters: {
+        products: []
+      },
+      res: [],
+      isUpperToolbarDropdownOpen: false,
+      isUpperToolbarKebabDropdownOpen: false,
+      isLowerToolbarDropdownOpen: false,
+      isLowerToolbarKebabDropdownOpen: false,
+      isCardKebabDropdownOpen: false,
+      activeItem: 0
+    };
+
+    this.onPageDropdownToggle = isUpperToolbarDropdownOpen => {
+      this.setState({
+        isUpperToolbarDropdownOpen
+      });
+    };
+
+    this.onPageDropdownSelect = event => {
+      this.setState({
+        isUpperToolbarDropdownOpen: !this.state.isUpperToolbarDropdownOpen
+      });
+    };
+
+    this.onPageToolbarDropdownToggle = isPageToolbarDropdownOpen => {
+      this.setState({
+        isPageToolbarDropdownOpen
+      });
+    };
+
+    this.onPageToolbarKebabDropdownToggle = isUpperToolbarKebabDropdownOpen => {
+      this.setState({
+        isUpperToolbarKebabDropdownOpen
+      });
+    };
+
+    this.onToolbarDropdownToggle = isLowerToolbarDropdownOpen => {
+      this.setState(prevState => ({
+        isLowerToolbarDropdownOpen
+      }));
+    };
+
+    this.onToolbarDropdownSelect = event => {
+      this.setState({
+        isLowerToolbarDropdownOpen: !this.state.isLowerToolbarDropdownOpen
+      });
+    };
+
+    this.onToolbarKebabDropdownToggle = isLowerToolbarKebabDropdownOpen => {
+      this.setState({
+        isLowerToolbarKebabDropdownOpen
+      });
+    };
+
+    this.onToolbarKebabDropdownSelect = event => {
+      this.setState({
+        isLowerToolbarKebabDropdownOpen: !this.state.isLowerToolbarKebabDropdownOpen
+      });
+    };
+
+    this.onNavSelect = result => {
+      this.setState({
+        activeItem: result.itemId
+      });
+    };
+
+    this.deleteItem = item => event => {
+      const filter = getter => val => getter(val) !== item.id;
+      this.setState({
+        res: this.state.res.filter(filter(({ id }) => id)),
+        selectedItems: this.state.selectedItems.filter(filter(id => id))
+      });
+    };
+
+    this.onNameSelect = (event, selection) => {
+      const checked = event.target.checked;
+      this.setState(prevState => {
+        const prevSelections = prevState.filters['products'];
+        return {
+          filters: {
+            ...prevState.filters,
+            ['products']: checked ? [...prevSelections, selection] : prevSelections.filter(value => value !== selection)
+          }
+        };
+      });
+    };
+
+    this.onDelete = (type = '', id = '') => {
+      if (type) {
+        this.setState(prevState => {
+          prevState.filters[type.toLowerCase()] = prevState.filters[type.toLowerCase()].filter(s => s !== id);
+          return {
+            filters: prevState.filters
+          };
+        });
+      } else {
+        this.setState({
+          filters: {
+            products: []
+          }
+        });
+      }
+    };
+
+    this.onCloseDrawerClick = () => {
+      this.setState({
+        activeCard: null,
+        isDrawerExpanded: false
+      });
+    };
+
+    this.onKeyDown = event => {
+      if (event.target !== event.currentTarget || event.currentTarget.id === this.state.activeCard) {
+        return;
+      }
+      if ([13, 32].includes(event.keyCode)) {
+        const newSelected = event.currentTarget.id;
+        this.setState({
+          activeCard: newSelected,
+          isDrawerExpanded: true
+        });
+      }
+    };
+
+    this.onCardClick = event => {
+      if (event.currentTarget.id === this.state.activeCard) {
+        return;
+      }
+
+      const newSelected = event.currentTarget.id;
+
+      this.setState({
+        activeCard: newSelected,
+        isDrawerExpanded: true
+      });
+    };
+
+    this.onPerPageSelect = (_evt, perPage) => {
+      this.setState({ page: 1, perPage })
+    }
+
+    this.onSetPage = (_evt, page) => {
+      this.setState({ page })
+    }
+  }
+
+  getAllItems() {
+    const { res } = this.state;
+    const collection = [];
+    for (const items of res) {
+      collection.push(items.id);
+    }
+
+    return collection;
+  }
+
+  fetch(page, perPage) {
+    fetch(`https://my-json-server.typicode.com/jenny-s51/cardviewdata/posts?_page=${page}&_limit=${perPage}`)
+      .then(resp => resp.json())
+      .then(resp => this.setState({ res: resp, perPage, page }))
+      .then(() => this.updateSelected())
+      .catch(err => this.setState({ error: err }));
+  }
+
+  componentDidMount() {
+    this.fetch(this.state.page, this.state.perPage);
+  }
+
+  buildFilterDropdown() {
+    const { isLowerToolbarDropdownOpen, filters } = this.state;
+
+    const filterDropdownItems = [
+      <SelectOption key="patternfly" value="Patternfly" />,
+      <SelectOption key="activemq" value="ActiveMQ" />,
+      <SelectOption key="apachespark" value="Apache Spark" />,
+      <SelectOption key="avro" value="Avro" />,
+      <SelectOption key="azureservices" value="Azure Services" />,
+      <SelectOption key="crypto" value="Crypto" />,
+      <SelectOption key="dropbox" value="DropBox" />,
+      <SelectOption key="jbossdatagrid" value="JBoss Data Grid" />,
+      <SelectOption key="rest" value="REST" />,
+      <SelectOption key="swagger" value="SWAGGER" />
+    ];
+
+    return (
+      <ToolbarFilter categoryName="Products" chips={filters.products} deleteChip={this.onDelete}>
+        <Select
+          variant={SelectVariant.checkbox}
+          aria-label="Products"
+          onToggle={this.onToolbarDropdownToggle}
+          onSelect={this.onNameSelect}
+          selections={filters.products}
+          isExpanded={isLowerToolbarDropdownOpen}
+          placeholderText="Creator"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {filterDropdownItems}
+        </Select>
+      </ToolbarFilter>
+    );
+  }
+
+  render() {
+    const {
+      isDrawerExpanded,
+      activeCard,
+      isUpperToolbarDropdownOpen,
+      isLowerToolbarDropdownOpen,
+      isUpperToolbarKebabDropdownOpen,
+      isLowerToolbarKebabDropdownOpen,
+      isCardKebabDropdownOpen,
+      activeItem,
+      filters,
+      res
+    } = this.state;
+
+    const toolbarKebabDropdownItems = [
+      <DropdownItem key="link">Link</DropdownItem>,
+      <DropdownItem key="action" component="button">
+        Action
+      </DropdownItem>,
+      <DropdownItem key="disabled link" isDisabled>
+        Disabled Link
+      </DropdownItem>,
+      <DropdownItem key="disabled action" isDisabled component="button">
+        Disabled Action
+      </DropdownItem>,
+      <DropdownSeparator key="separator" />,
+      <DropdownItem key="separated link">Separated Link</DropdownItem>,
+      <DropdownItem key="separated action" component="button">
+        Separated Action
+      </DropdownItem>
+    ];
+
+    const toolbarItems = (
+      <React.Fragment>
+        <ToolbarItem variant="overflow-menu">
+          <OverflowMenu breakpoint="xl">
+            <OverflowMenuItem isPersistent>{this.buildFilterDropdown()}</OverflowMenuItem>
+            <OverflowMenuItem isPersistent>
+              <Button variant="primary">Create a Project</Button>
+            </OverflowMenuItem>
+            <OverflowMenuControl hasAdditionalOptions>
+              <Dropdown
+                onSelect={this.onToolbarKebabDropdownSelect}
+                toggle={<KebabToggle onToggle={this.onToolbarKebabDropdownToggle} id="card-view-data-toolbar-dropdown" />}
+                isOpen={isLowerToolbarKebabDropdownOpen}
+                isPlain
+                dropdownItems={toolbarKebabDropdownItems}
+              />
+            </OverflowMenuControl>
+          </OverflowMenu>
+        </ToolbarItem>
+      </React.Fragment>
+    );
+
+    const PageNav = (
+      <Nav onSelect={this.onNavSelect} aria-label="Nav">
+        <NavList>
+          <NavItem itemId={0} isActive={activeItem === 0}>
+            System Panel
+          </NavItem>
+          <NavItem itemId={1} isActive={activeItem === 1}>
+            Policy
+          </NavItem>
+          <NavItem itemId={2} isActive={activeItem === 2}>
+            Authentication
+          </NavItem>
+          <NavItem itemId={3} isActive={activeItem === 3}>
+            Network Services
+          </NavItem>
+          <NavItem itemId={4} isActive={activeItem === 4}>
+            Server
+          </NavItem>
+        </NavList>
+      </Nav>
+    );
+
+    const userDropdownItems = [
+      <DropdownItem>Link</DropdownItem>,
+      <DropdownItem component="button">Action</DropdownItem>,
+      <DropdownItem isDisabled>Disabled Link</DropdownItem>,
+      <DropdownItem isDisabled component="button">
+        Disabled Action
+      </DropdownItem>,
+      <DropdownSeparator />,
+      <DropdownItem>Separated Link</DropdownItem>,
+      <DropdownItem component="button">Separated Action</DropdownItem>
+    ];
+
+    const PageToolbar = <div>need to implement new toolbar</div>;
+
+    const Header = (
+      <PageHeader
+        logo={<Brand src={imgBrand} alt="Patternfly Logo" />}
+        toolbar={PageToolbar}
+        avatar={<Avatar src={imgAvatar} alt="Avatar image" />}
+        showNavToggle
+      />
+    );
+    const Sidebar = <PageSidebar nav={PageNav} />;
+    const pageId = 'main-content-card-view-default-nav';
+    const PageSkipToContent = <SkipToContent href={`#${pageId}`}>Skip to Content</SkipToContent>;
+
+    const filtered =
+      filters.products.length > 0
+        ? res.filter(card => {
+            return filters.products.length === 0 || filters.products.includes(card.name);
+          })
+        : res;
+
+    const icons = {
+      pfIcon,
+      activeMQIcon,
+      sparkIcon,
+      avroIcon,
+      azureIcon,
+      saxonIcon,
+      dropBoxIcon,
+      infinispanIcon,
+      restIcon,
+      swaggerIcon
+    };
+
+    const drawerContent = (
+      <Gallery hasGutter>
+        {filtered.map((product, key) => (
+          <React.Fragment>
+            <Card
+              isHoverable
+              key={key}
+              id={'card-view-' + key}
+              onKeyDown={this.onKeyDown}
+              onClick={this.onCardClick}
+              isSelectable
+              isSelected={activeCard === key}
+            >
+              <CardHeader>
+                <img src={icons[product.icon]} alt={`${product.name} icon`} style={{ height: '50px' }} />
+              </CardHeader>
+              <CardTitle>{product.name}</CardTitle>
+              <CardBody>{product.description}</CardBody>
+            </Card>
+          </React.Fragment>
+        ))}
+      </Gallery>
+    );
+
+    const panelContent = (
+      <DrawerPanelContent>
+        <DrawerHead>
+          <Title headingLevel="h2" size="xl">
+            node-{activeCard && activeCard.charAt(activeCard.length - 1)}
+          </Title>
+          <DrawerActions>
+            <DrawerCloseButton onClick={this.onCloseDrawerClick} />
+          </DrawerActions>
+        </DrawerHead>
+        <DrawerPanelBody>
+          <Flex spaceItems={{ default: 'spaceItemsLg' }} direction={{ default: 'column' }}>
+            <FlexItem>
+              <p>
+                The content of the drawer really is up to you. It could have form fields, definition lists, text lists,
+                labels, charts, progress bars, etc. Spacing recommendation is 24px margins. You can put tabs in here,
+                and can also make the drawer scrollable.
+              </p>
+            </FlexItem>
+            <FlexItem>
+              <Progress value={activeCard * 10} title="Title" />
+            </FlexItem>
+            <FlexItem>
+              <Progress value={activeCard * 5} title="Title" />
+            </FlexItem>
+          </Flex>
+        </DrawerPanelBody>
+      </DrawerPanelContent>
+    );
+
+    return (
+      <React.Fragment>
+        <Page
+          header={Header}
+          sidebar={Sidebar}
+          isManagedSidebar
+          skipToContent={PageSkipToContent}
+          mainContainerId={pageId}
+        >
+          <PageSection variant={PageSectionVariants.light}>
+            <TextContent>
+              <Text component="h1">Projects</Text>
+              <Text component="p">This is a demo that showcases Patternfly Cards.</Text>
+            </TextContent>
+          </PageSection>
+          <PageSection isFilled>
+            <Drawer isExpanded={isDrawerExpanded} className={'pf-m-inline-on-2xl'}>
+              <DrawerSection className="pf-u-mb-md">
+                <Divider component="div" />
+              </DrawerSection>
+              <DrawerContent panelContent={panelContent} className={'pf-m-no-background'}>
+                <DrawerContentBody>{drawerContent}</DrawerContentBody>
+              </DrawerContent>
+            </Drawer>
+          </PageSection>
+          <PageSection isFilled={false} sticky="bottom" padding={{default: "noPadding"}} variant="light">
+            <Pagination
+              itemCount={filtered.length}
+              page={this.state.page}
+              perPage={this.state.perPage}
+              onPerPageSelect={this.onPerPageSelect}
+              onSetPage={this.onSetPage}
+              variant="bottom"
+            />
+          </PageSection>
+        </Page>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
